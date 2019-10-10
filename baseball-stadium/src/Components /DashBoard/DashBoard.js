8a67c78432d80a50c0d982cfc70594583f36cd37
lesley <lesleybanadzem2017@gmail.com>
2019-10-10 11:15:08 -0700

    Implimented the coubnt rules so the buttons work correctly

--- baseball-stadium/src/Components /DashBoard/DashBoard.js
+++ baseball-stadium/src/Components /DashBoard/DashBoard.js
@@ -1,16 +1,14 @@
-//provide a button that the person in charge can press every time there is a strike, ball, foul or hit.
-// there is NO need to specify the type of hit (single, double, etc).
-// changes recorded on this component should update the information shown by the Display component.
+
 import React from 'react';
 import styled from 'styled-components';
 
-function DashBoard() {
+function DashBoard(props) {
     const btnText =['strike', 'ball', 'foul', 'hit']
     return (
 
         <BtnStyles>
             {btnText.map( btn => { 
-                return <button key = { btn } > { btn } </button>
+                return <button key = { btn } onClick={e=>{props.handleClick( e, btn)}} > { btn } </button>
             })}
         </BtnStyles>
     )
--- baseball-stadium/src/Components /Display/Display.js
+++ baseball-stadium/src/Components /Display/Display.js
@@ -1,23 +1,60 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 import DashBoard from '../DashBoard/DashBoard';
-// display the count of balls and strikes for the at-bat.
-// should be updated when the user records activity on the Dashboard component.
 
 function Display() {
     const [ball, setBall] = useState(0);
     const [strike, setStrike] = useState(0);
     const [out, setOut] = useState(0);
-
+    const [hit, setHit] = useState(0);
+    useEffect(() => {
+    }, [ball, strike, out])
+    const handleClick = ( e, state ) => {
+        e.preventDefault();
+        if(state === 'ball'){
+           if(ball >= 4) {
+               setBall(0)
+               setStrike(0)} 
+            else{ 
+                setBall(ball + 1);
+            }
+        }
+        else if(state === 'strike'){
+            if(strike >= 3) {
+                setBall(0)
+                setStrike(0)} 
+             else{ 
+                 setStrike(strike + 1);
+             }
+        }
+        else if(state === 'hit'){
+                setBall(0)
+                setStrike(0)
+                setHit(hit + 1)
+        } else if (state === 'foul'){
+            if(strike === 0){
+                setStrike(strike +  1);
+            } else if(strike === 1) {
+                setStrike(strike + 2);
+            } else if(strike === 2){
+                setStrike(strike)
+            } else{
+                setStrike(0)
+            }
+        }
+    }
     return (
         <div style={{width: '700px', margin: '0 auto'}}>
             <ScoreboardStyles>
                 <div className='ball'><h3>Ball: {ball}</h3></div>
                 <div className='strike'><h3>Strikes: {strike}</h3></div>
                 <div className='Out'><h3>Out: {out}</h3></div>
+                <div className='Out'><h3>Hit: {hit}</h3></div>
             </ScoreboardStyles>
-            <DashBoard />
+            <DashBoard 
+                handleClick = {handleClick }
+            />
         </div>
     )
 }
